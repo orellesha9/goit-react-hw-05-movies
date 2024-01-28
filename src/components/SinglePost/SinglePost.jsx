263f582a02142f8a94b3cdff2bd40ef19767ea2b
orellesha9 <orellesha9@gmail.com>
2024-01-28 15:47:43 +0200

    start

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,40 +1,40 @@
-import styles from './single-post.module.css';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getPostById } from 'api/post';
+// import styles from './single-post.module.css';
+// import { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { getPostById } from 'api/post';
 
-const SinglePost = () => {
-  const [post, setPost] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const { id } = useParams();
+// const SinglePost = () => {
+//   const [post, setPost] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState();
+//   const { id } = useParams();
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        setLoading(true);
-        const { data } = await getPostById(id);
-        console.log(data);
-        setPost(data);
-      } catch {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-  }, []);
-  return (
-    <div>
-      {loading && <p>...Loading</p>}
-      {error && <p>Error: {error}</p>}
-      {post && (
-        <>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </>
-      )}
-    </div>
-  );
-};
+//   useEffect(() => {
+//     const fetchPost = async () => {
+//       try {
+//         setLoading(true);
+//         const { data } = await getPostById(id);
+//         console.log(data);
+//         setPost(data);
+//       } catch {
+//         setError(error.message);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//   }, []);
+//   return (
+//     <div>
+//       {loading && <p>...Loading</p>}
+//       {error && <p>Error: {error}</p>}
+//       {post && (
+//         <>
+//           <h2>{post.title}</h2>
+//           <p>{post.body}</p>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
 
-export default SinglePost;
+// export default SinglePost;
